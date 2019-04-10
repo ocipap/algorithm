ecd6948a2a082fb88a474f528a4c1e9694b94e5c
PAPICO <ocipap0531@gmail.com>
2019-04-10 11:06:48 +0900

    시험성적 문제풀이

---
+++ baekjoon/9468.py
@@ -0,0 +1,12 @@
+grade = int(input())
+
+if grade > 89 :
+    print("A")
+elif grade > 79 :
+    print("B")
+elif grade > 69 :
+    print("C")
+elif grade > 59 :
+    print("D")
+else :
+    print("F")
