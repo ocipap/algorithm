d4d09b2c8c8f92827322b5caa5658bd137a85280
PAPICO <ocipap0531@gmail.com>
2019-04-25 02:12:38 +0900

    fix: 2×n 타일링 2  런타임 에러 해결

--- baekjoon/11727.py
+++ baekjoon/11727.py
@@ -1,8 +1,11 @@
 n = int(input())
 arr = [0] * (n+1)
-arr[1] = 1
-arr[2] = 3
-for i in range(3, n+1):
-    arr[i] = arr[i-1] + arr[i-2] * 2
+for i in range(1, n+1):
+    if i == 1 :
+        arr[i] = 1
+    elif i == 2 :
+        arr[i] = 3
+    else :
+        arr[i] = arr[i-1] + arr[i-2] * 2
 
 print(arr[n] % 10007)
