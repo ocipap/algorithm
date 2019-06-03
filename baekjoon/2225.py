b439b45e23a75299b4fc7c70806cb0591dc51fde
papico <ocipap0531@gmail.com>
2019-06-03 22:22:12 +0900

    합분해 문제해결

---
+++ baekjoon/2225.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+dp = [0] * (n+1)
+dp[0] = 1
+
+for i in range(1, k+1):
+  for j in range(0, n+1):
+    if i == 1 or j == 0:
+      dp[j] = 1
+    else :
+      dp[j] += dp[j-1]
+  dp[j] %= 1000000000
+
+print(dp[j])
